e701af622cd35c5c256c7276eb4f97559612ea7f
Vicente <vicenntte@outlook.es>
2024-09-08 19:36:43 -0600

    Posición de cada Aruco en ROS
    Ya están los 3 arucos en una pose del array dentro del topico /arucos, solo faltan funcionalidades extra que vayamos a implementar y cualquier cosa extra del reto

--- pos_aruco.m
+++ pos_aruco.m
@@ -1,10 +1,38 @@
-cam = webcam(1);
+cam = webcam(2);
 
 % Paso 1: Cargar los parámetros de calibración
 load('parametros_laptop.mat', 'params');
 
+setenv("ROS_DOMAIN_ID","0")
+%ros2 node list
+%ros2 topic list
+i = 0;
+
+testnode = ros2node("/pc_externa");
+aruco_pub = ros2publisher(testnode,"/arucos_pose","geometry_msgs/PoseArray");
+aruco_array = ros2message("geometry_msgs/PoseArray");
+aruco0 = ros2message('geometry_msgs/Pose');
+aruco1 = ros2message('geometry_msgs/Pose');
+aruco2 = ros2message('geometry_msgs/Pose');
+
+aruco0.position.x = 0;
+aruco0.position.y = 0;
+aruco0.orientation.z = 0;
+
+aruco1.position.x = 1;
+aruco1.position.y = 1;
+aruco1.orientation.z = 1;
+
+aruco2.position.x = 2;
+aruco2.position.y = 2;
+aruco2.orientation.z = 2;
+
+aruco_array.header.frame_id ='arucos';
+aruco_array.poses = [aruco0, aruco1, aruco2];
+
+
 while true
-    pause(0.25); 
+    %pause(0.25); 
     % Capturar una imagen de la cámara
     I = snapshot(cam);
 
@@ -13,12 +41,13 @@ while true
     numMarkers = length(ids);
 
     for i = 1:numMarkers
-        pause(0.25)
+        %pause(0.25)
         loc = locs(:,:,i);
 
         % Mostrar el ID del marcador y la familia
-        disp("Detected marker ID, Family: " + ids(i) + ", " + detectedFamily(i));  
-        
+        %disp("Detected marker ID, Family: " + ids(i) + ", " + detectedFamily(i));  
+        disp(ids(i));
+        arucoID = ids(i);
         % Insertar bordes del marcador
         I = insertShape(I,"polygon",{loc},Opacity=1,ShapeColor="green",LineWidth=4);
         
@@ -37,8 +66,8 @@ while true
 
         % Mostrar el centro en coordenadas reales
         %fprintf('Centro del marcador ID: %d\n', ids(i));
-        fprintf('Coordenadas en el mundo real (X, Y) en mm: %.2f, %.2f\n', ...
-                centerWorld(1), centerWorld(2));
+        % fprintf('Coordenadas en el mundo real (X, Y) en mm: %.2f, %.2f\n', ...
+        %         centerWorld(1), centerWorld(2));
 
         % Obtener el ángulo de inclinación del marcador
         % Definir el vector entre dos esquinas consecutivas del marcador en píxeles
@@ -53,11 +82,41 @@ while true
 
         % Insertar los IDs del marcador en la imagen
         I = insertText(I, center, ids(i), FontSize=30, BoxOpacity=1);
+
+        
+        % En este switch se le asigna la posición a cada aruco del topico de ROS
+        switch arucoID
+            case 0
+                aruco0.position.x = centerWorld(1);
+                aruco0.position.y = centerWorld(2);
+                aruco0.orientation.z = angleDegrees;
+            case 1
+                aruco1.position.x = centerWorld(1);
+                aruco1.position.y = centerWorld(2);
+                aruco1.orientation.z = angleDegrees;
+            case 2
+                aruco2.position.x = centerWorld(1);
+                aruco2.position.y = centerWorld(2);
+                aruco2.orientation.z = angleDegrees;
+            otherwise
+                disp('Lectura falsa');
+        end
+
+
+
+
+
+
+
+        aruco_array.poses = [aruco0, aruco1, aruco2];
+        send(aruco_pub, aruco_array);
+
+        
     end
     
     % Mostrar la imagen procesada con las anotaciones
+
     imshow(I);
-    
     % Mostrar la posición del centro del marcador en píxeles (opcional)
     %disp(center);
 end
