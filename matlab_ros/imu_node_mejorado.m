e47f860e263b3311917d7daadb4a45f3daa8d520
Vicente <vicenntte@outlook.es>
2024-09-09 20:58:47 -0600

    Guardar la aceleración para luego graficar
    Hay que correr el código mientrás se está conectado al turtlebot y dejar que data se llene de algunos datos, luego desconectarse del turtlebot y graficar con plot(data.time,data.ax)

--- matlab_ros/imu_node_mejorado.m
+++ matlab_ros/imu_node_mejorado.m
@@ -1,20 +1,27 @@
-function [] = imu_callback(msg)
-        orient = msg.orientation; 
-        w = msg.angular_velocity;
-        acceleration = msg.linear_acceleration;
-        %disp(orient)
-        %disp(w)
-        disp(acceleration)
-end
+% Inicializar la variable global para almacenar datos
+global data;
+data = struct('time', [], 'ax', [], 'ay', [], 'az', []);
 
-setenv("ROS_DOMAIN_ID","0")
-%ros2 node list
-%ros2 topic list
-
-
+% Configuración del entorno ROS y el suscriptor
+setenv("ROS_DOMAIN_ID", "0");
 testnode = ros2node("/node_imu");
-%testpub = ros2publishetestnode,"/cmd_vel","geometry_msgs/Twist");
-%chatterMSG = ros2message("geometry_msclgs/Twist");
-imuSub = ros2subscriber(testnode,"/imu",@imu_callback);
-%while(1)
-%end
+imuSub = ros2subscriber(testnode, "/imu", @imu_callback);
+
+% Función de callback para almacenar los datos
+function [] = imu_callback(msg)
+    global data;
+    
+    % Acceder a los datos del mensaje
+    ax = msg.linear_acceleration.x;
+    ay = msg.linear_acceleration.y;
+    az = msg.linear_acceleration.z;
+    
+    % Obtener el tiempo actual
+    t = datetime('now');
+    
+    % Almacenar los datos
+    data.time(end+1) = datenum(t);
+    data.ax(end+1) = ax;
+    data.ay(end+1) = ay;
+    data.az(end+1) = az;
+end
