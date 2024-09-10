adf8cb5eef87b8e32e1020c83252cc5d730dbfed
Vicente <vicenntte@outlook.es>
2024-09-09 21:02:07 -0600

    ya funciona de manera correcta
    Se corre de manera normal, solo hay que escribir clear en la terminal de matlab para acabar el proceso

--- matlab_ros/cam_node.m
+++ matlab_ros/cam_node.m
@@ -1,19 +1,4 @@
-% function img_callback(msg)
-%     while(1)
-%         img = rosReadImage(msg);
-%         ros2 topic list
-%     end
-% end
-% setenv("ROS_DOMAIN_ID","0")
-% %ros2 node list
-% %ros2 topic list
-% i = 0;
-% 
-% testnode = ros2node("/nodo_cam");
-% %testpub = ros2publisher(testnode,"/cmd_vel","geometry_msgs/Twist");
-% %chatterMSG = ros2message("geometry_msgs/Twist");
-% camSub = ros2subscriber(testnode,"/image_raw","sensor_msgs/Image",img_callback);
-
+clear all
 
 function [] = img_callback(msg)
         img = rosReadImage(msg);
@@ -22,14 +7,8 @@ function [] = img_callback(msg)
 end
 
 setenv("ROS_DOMAIN_ID","0")
-%ros2 node list
-%ros2 topic list
 i = 0;
 
 testnode = ros2node("/nodo_cam");
-%testpub = ros2publisher(testnode,"/cmd_vel","geometry_msgs/Twist");
-%chatterMSG = ros2message("geometry_msgs/Twist");
 camSub = ros2subscriber(testnode,"/image_raw",@img_callback);
-while(1)
-end
 
